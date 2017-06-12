507482e090b4de8bb05fd40b3a5d7af27f8982de
DForbes <damianoforbes@gmail.com>
2017-06-12 13:30:03 -0400

    Add files via upload

---
+++ Django/eCommerce/profiles/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class profile(models.Model):
+	name = models.CharField(max_length=120)
+	description = models.TextField(default='description default text')
+	
+	# Used as demos
+	#location = models.CharField(max_length=120, default='my location default',blank=True, null=True)
+	#job = models.CharField(max_length=120, null=True)
+	
+	def __unicode__(self):
+		return self.name
